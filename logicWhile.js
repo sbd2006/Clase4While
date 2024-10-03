bab32b50a2c3c87f4f412c2e4fa29fc9f3c47dca
samuelbeltran048@gmail.com <samuelbeltran048@gmail.com>
2024-10-03 08:15:15 -0500

    Clase while

---
+++ logicWhile.js
@@ -0,0 +1,12 @@
+var numeroEntrada = 0;
+var resultado = 0;
+var control = 0;
+
+numeroEntrada = parseInt(prompt("Ingrese el numero  que desea calcular: "));
+
+while (numeroEntrada>control) {
+    resultado = resultado + control;
+    control = control + 1;
+}
+
+alert("La suma de los primeros " +numeroEntrada+ " naturales es igula a " +resultado);
